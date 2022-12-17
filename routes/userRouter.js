aca36b901ce2b07f1541e469a4720b660af5fa26
MonikaPandian <panmonikmm@gmail.com>
2022-12-17 16:15:54 +0530

    vercel.json added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { client } from "../index.js";
@@ -18,10 +18,14 @@ router.post("/sign-up", async (request, response) => {
         return;
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt)
-    const result = await client.db("inventoryBilling").collection("users").insertOne({ username: username, firstName: firstName, lastName: lastName, password: hashedPassword });
-    response.send(result)
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const result = await client.db("inventoryBilling").collection("users").insertOne({ username: username, firstName: firstName, lastName: lastName, password: hashedPassword });
+        response.send(result)
+    } catch (error) {
+        response.status(500).send("Internal server error");
+    }
 })
 
 //login 
@@ -34,16 +38,20 @@ router.post("/login", async (request, response) => {
         return;
     }
 
-    const storedPassword = userFromDB.password;
+    try {
+        const storedPassword = userFromDB.password;
 
-    const isPasswordMatch = await bcrypt.compare(password, storedPassword)
-    if (!isPasswordMatch) {
-        response.status(400).send({ message: "Invalid credentials" });
-        return;
+        const isPasswordMatch = await bcrypt.compare(password, storedPassword)
+        if (!isPasswordMatch) {
+            response.status(400).send({ message: "Invalid credentials" });
+            return;
+        }
+        //issue token
+        const token = jwt.sign({ id: userFromDB._id }, process.env.SECRET_KEY);
+        response.send({ message: "Successful login", token: token, username: userFromDB.username, id: userFromDB._id, isAdmin: false, firstName: userFromDB.firstName, lastName: userFromDB.lastName });
+    } catch (error) {
+        response.status(500).send("Internal server error")
     }
-    //issue token
-    const token = jwt.sign({ id: userFromDB._id }, process.env.SECRET_KEY);
-    response.send({ message: "Successful login", token: token, username: userFromDB.username, id: userFromDB._id, isAdmin: false, firstName: userFromDB.firstName, lastName: userFromDB.lastName });
 })
 
 //forgot-password send-email
@@ -56,43 +64,40 @@ router.post("/forgot-password", async (request, response) => {
         response.status(400).send({ message: "Enter a valid and registered email Id" })
         return;
     }
-    //User exist and now create a one time link valid for 15 minutes
-    const secret = process.env.SECRET_KEY + userFromDB.password;
-   
-    const payload = {
-        email: userFromDB.username,
-        id: userFromDB._id
-    }
+    try {
+        //User exist and now create a one time link valid for 15 minutes
+        const secret = process.env.SECRET_KEY + userFromDB.password;
 
-    const token = jwt.sign(payload, secret, { expiresIn: '15m' })
-    const link = `https://inventory-billing-app-751e91.netlify.app/reset-password/${userFromDB._id}/${token}`;
-
-    var transporter = NodeMailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'panmonikmm@gmail.com',
-            pass: 'lxkugchepioxgtmr'
+        const payload = {
+            email: userFromDB.username,
+            id: userFromDB._id
         }
-    });
 
-    var mailOptions = {
-        from: 'panmonikmm@gmail.com',
-        to: `${userFromDB.username}`,
-        subject: 'Inventory billing Application Password reset link',
-        html: `We have received your request for reset password. Click this link to reset your password.<br>
+        const token = jwt.sign(payload, secret, { expiresIn: '15m' })
+        const link = `https://inventory-billing-app-751e91.netlify.app/reset-password/${userFromDB._id}/${token}`;
+
+        var transporter = NodeMailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'panmonikmm@gmail.com',
+                pass: 'lxkugchepioxgtmr'
+            }
+        });
+
+        var mailOptions = {
+            from: 'panmonikmm@gmail.com',
+            to: `${userFromDB.username}`,
+            subject: 'Inventory billing Application Password reset link',
+            html: `We have received your request for reset password. Click this link to reset your password.<br>
               <a style="font-size:20px" href = ${link}>Click Here</a><br>
               <p>This link is valid for 15 minutes from your request initiation for password recovery.</p>`
-    };
+        };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            console.log('Email sent:' + info.response);
-        }
-    })
-    response.send({ message: "success" });
+        const message = await transporter.sendMail(mailOptions).then((response) => console.log(response)).catch((error) => console.log(error));
+        response.send({ message: "success" });
+    } catch (error) {
+        response.status(500).send("Internal server error");
+    }
 })
 
 //reset password
@@ -121,7 +126,7 @@ router.post("/reset-password/:id/:token", async (request, response) => {
 
     //check if this id exist in database
     const userFromDB = await client.db("inventoryBilling").collection("users").findOne({ _id: ObjectId(id) })
-  
+
     if (!userFromDB) {
         response.status(400).send({ message: "User not exists!!" })
         return;
